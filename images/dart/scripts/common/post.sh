9e7cf3b7f972c433b1705a7f9d927f0967d12074
Felix Barz <Skycoder42@users.noreply.github.com>
2023-06-13 06:46:03 +0200

    Use cider extensions of dart_test_tools

--- images/dart/scripts/common/post.sh
+++ images/dart/scripts/common/post.sh
@@ -5,6 +5,6 @@ set -eo pipefail
 binary="$1"
 
 sudo -u vscode "$binary" pub global activate melos
-sudo -u vscode "$binary" pub global activate cider
+sudo -u vscode "$binary" pub global activate dart_test_tools -x cider
 
 rm -rf /tmp/scripts
